4d69e9cbc00b37ed0e11404052d3eb564efba850
Adriano Ettari <adrianoettari@libero.it>
2024-04-04 16:11:44 +0200

    no more EMA

--- train_diffusion_superres.py
+++ train_diffusion_superres.py
@@ -14,7 +14,7 @@ from torch.utils.data import DataLoader, Dataset
 from utils import get_data_superres
 import copy
 
-from SISR.UNet_model_SISR import Residual_Attention_UNet_superres, Attention_UNet_superres
+from UNet_model_superres_new import Residual_Attention_UNet_superres, Attention_UNet_superres
 
 import torch
 import torch.nn as nn
@@ -93,16 +93,23 @@ class CombinedLoss_MSE_PercLoss(nn.Module):
         else:
             return MSE_loss_value
 
-class SISR_UNet():
+class Diffusion:
     def __init__(
             self,
+            noise_schedule: str,
             model: nn.Module,
             snapshot_path: str,
+            noise_steps=1000,
+            beta_start=1e-4,
+            beta_end=0.02,
             magnification_factor=4,
             device='cuda',
             image_size=224,
-            model_name='UNet superres'):
-
+            model_name='superres'):
+    
+        self.noise_steps = noise_steps
+        self.beta_start = beta_start
+        self.beta_end = beta_end
         self.image_size = image_size
         self.model_name = model_name
         self.magnification_factor = magnification_factor
@@ -117,6 +124,153 @@ class SISR_UNet():
 
         # epoch_run is used by _save_snapshot and _load_snapshot to keep track of the current epoch (MAYBE WE CAN REMOVE IT FROM HERE)
         self.epochs_run = 0
+
+        self.noise_schedule = noise_schedule
+
+        if self.noise_schedule == 'linear':
+            self.beta = self.prepare_noise_schedule().to(self.device) 
+            self.alpha = 1. - self.beta
+            self.alpha_hat = torch.cumprod(self.alpha, dim=0) # Notice that beta is not just a number. It is a tensor of shape (noise_steps,).
+        # If we are in the step t then we index the tensor with t. To get alpha_hat we compute the cumulative product of the tensor.
+
+        elif self.noise_schedule == 'cosine':
+            self.alpha_hat = self.prepare_noise_schedule().to(self.device)
+            self.beta = self.from_alpha_hat_to_beta()
+            self.alpha = 1. - self.beta
+
+    def from_alpha_hat_to_beta(self):
+        '''
+        This function is necessary because it allows to get from the alpha hat that we got with the cosine schedule
+        the alpha and the beta which are necessary in order to compute the denoised image during sampling.
+        The reason we need this function is that with the linear schedule we start from beta, then we calculate alpha and so
+        alpha hat, whereas with the cosine schedule we start from alpha hat, then we must calculate beta and so alpha
+        because we need them to compute the denoised image.
+
+        Input:
+            alpha_hat: a tensor of shape (noise_steps,) that contains the alpha_hat values for each noise step.
+        
+        Output:
+            beta: a tensor of shape (noise_steps,) that contains the beta values for each noise step.
+        '''
+        beta = []
+        for t in range(len(self.alpha_hat)-1, 0, -1):
+            beta.append(1 -(self.alpha_hat[t]/self.alpha_hat[t-1]))
+        beta.append(1 - self.alpha_hat[0])
+        beta =  torch.tensor(beta[::-1], dtype=self.alpha_hat.dtype, device=self.alpha_hat.device)
+        return beta
+
+    def prepare_noise_schedule(self):
+        '''
+        In this function we set the noise schedule to use. Basically, we need to know how much gaussian noise we want to add
+        for each noise step.
+
+        Input:
+            noise_schedule: the name of the noise schedule to use. It can be either 'linear' or 'cosine'.
+
+        Output:
+            if noise_schedule == 'linear':
+                self.beta: a tensor of shape (noise_steps,) that contains the beta values for each noise step.
+            elif noise_schedule == 'cosine':
+                self.alpha_hat: a tensor of shape (noise_steps,) that contains the alpha_hat values for each noise step.
+        '''
+        if self.noise_schedule == 'linear':
+            return torch.linspace(self.beta_start, self.beta_end, self.noise_steps)
+        elif self.noise_schedule == 'cosine':
+            f_t = torch.cos(((((torch.arange(self.noise_steps)/self.noise_steps)+0.008)/(1+0.008))*torch.pi/2))**2 # Here we apply the formula of the OpenAI paper https://arxiv.org/pdf/2102.09672.pdf
+            alpha_hat = f_t/f_t[0]  
+            return alpha_hat
+
+    def noise_images(self, x, t):
+        '''
+        ATTENTION: The error epsilon is random, but how much of it we add to move forward depends on the Beta schedule.
+
+        Input:
+            x: the image at time t=0
+            t: the current timestep
+        
+        Output:
+            x_t: the image at the current timestep (x_t)
+            epsilon: the error that we add to x_t to move forward
+        '''
+        sqrt_alpha_hat = torch.sqrt(self.alpha_hat[t])[:, None, None, None] # Each None is a new dimension (e.g.
+        # if a tensor has shape (2,3,4), a[None,None,:,None] will be shaped (1,1,2,1,3,4)). It doens't add
+        # them exatly in the same place, but it adds them in the place where the None is.
+        sqrt_one_minus_alpha_hat = torch.sqrt(1 - self.alpha_hat[t])[:, None, None, None]
+        epsilon = torch.randn_like(x, dtype=torch.float32) # torch.randn_like() returns a tensor of the same shape of x with random values from a standard gaussian
+        # (notice that the values inside x are not relevant)
+        # epsilon = 2 * (epsilon - epsilon.min()) / (epsilon.max() - epsilon.min()) - 1
+        return sqrt_alpha_hat * x + sqrt_one_minus_alpha_hat * epsilon, epsilon
+
+    def sample_timesteps(self, n):
+        '''
+        During the training we sample t from a Uniform discrete distribution (from 1 to T)
+
+        For each image that I have in the training, I want to sample a timestep t from a uniform distribution
+        (notice that it is not the same for each image). 
+
+        Input:
+            n: the number of images we want to sample the timesteps for
+
+        Output:
+            t: a tensor of shape (n,) that contains the timesteps for each image
+        '''
+        return torch.randint(low=1, high=self.noise_steps, size=(n,))
+    
+    def sample(self, n, model, lr_img, input_channels=3, plot_gif_bool=False):
+        '''
+        As the name suggests this function is used for sampling. Therefore we want to 
+        loop backward (moreover, notice that in the sample we want to perform EVERY STEP CONTIGUOUSLY).
+
+        What we do is to predict the noise conditioned by the time step and by the low resolution image.
+
+        Input:
+            n: the number of images we want to sample
+            lr_img: the low resolution image
+            input_channels: the number of input channels
+            plot_gif_bool: if True, the function will plot a gif with the generated images for each class
+        
+        Output:
+            x: a tensor of shape (n, input_channels, self.image_size, self.image_size) with the generated images
+        '''
+        lr_img = lr_img.to(self.device).unsqueeze(0)
+
+        frames = []
+        model.eval() # disables dropout and batch normalization
+        with torch.no_grad(): # disables gradient calculation
+            x = torch.randn((n, input_channels, self.image_size, self.image_size))
+            # x = x-x.min()/(x.max()-x.min()) # normalize the values between 0 and 1
+            x = x.to(self.device) # generates n noisy images of shape (3, self.image_size, self.image_size)
+            for i in tqdm(reversed(range(1, self.noise_steps)), position=0): 
+                t = (torch.ones(n) * i).long().to(self.device) # tensor of shape (n) with all the elements equal to i.
+                # Basically, each of the n image will be processed with the same integer time step t.
+                
+                predicted_noise = model(x, t, lr_img, self.magnification_factor)
+
+                alpha = self.alpha[t][:, None, None, None]
+                alpha_hat = self.alpha_hat[t][:, None, None, None]
+                beta = self.beta[t][:, None, None, None]
+                if i > 1:
+                    # If i>1 then we add noise to the image we have sampled (remember that from x_t we sample x_{t-1}).
+                    # If i==1 we sample x_0, which is the final image we want to generate, so we don't add noise.
+                    noise = torch.randn_like(x)
+                else:
+                    noise = torch.zeros_like(x) # we don't add noise (it's equal to 0) in the last time step because it would just make the final outcome worse.
+                x = 1 / torch.sqrt(alpha) * (x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(beta) * noise
+                if plot_gif_bool == True:
+                    plt.imshow(x[0][0].cpu().numpy())
+                    plt.savefig(os.path.join(os.getcwd(), 'models_run', self.model_name, 'results', f'frame_{i}.png'))
+                    plt.title(f't-step={i}', fontsize=30)
+                    frames.append(imageio.imread(os.path.join(os.getcwd(), 'models_run', self.model_name, 'results', f'frame_{i}.png')))
+                    os.remove(os.path.join(os.getcwd(), 'models_run', self.model_name, 'results', f'frame_{i}.png'))
+        if plot_gif_bool == True:
+            imageio.mimsave(os.path.join(os.getcwd(), 'models_run', self.model_name, 'results', 'gif_result.gif'), frames, duration=0.25) 
+        model.train() # enables dropout and batch normalization
+        # x = (x.clamp(-1, 1) + 1) / 2 # clamp takes a minimum and a maximum. All the terms that you pass
+        # as input to it are then modified: if their are less than the minimum, clamp outputs the minimum, 
+        # otherwise outputs them. The same (but opposit reasoning) for the maximum.
+        # +1 and /2 just to bring the values back to 0 to 1.
+        # x = (x * 255).type(torch.uint8)
+        return x
 
     def _save_snapshot(self, epoch, model):
         '''
@@ -194,8 +348,6 @@ class SISR_UNet():
             vgg_loss = VGGPerceptualLoss(self.device)
             mse_loss = nn.MSELoss()
             loss_function = CombinedLoss_MSE_PercLoss(MSE_loss=mse_loss, Perc_loss=vgg_loss, weight_MSE_loss=0.3)
-        elif loss == 'Perceptual':
-            loss_function = VGGPerceptualLoss(self.device)
         
         epochs_without_improving = 0
         best_loss = float('inf')  
@@ -219,11 +371,18 @@ class SISR_UNet():
                 lr_img = lr_img.to(self.device)
                 hr_img = hr_img.to(self.device)
 
+                t = self.sample_timesteps(hr_img.shape[0]).to(self.device)
+                # t is a unidimensional tensor of shape (images.shape[0] that is the batch_size) with random integers from 1 to noise_steps.
+                x_t, noise = self.noise_images(hr_img, t) # get batch_size noise images
+
                 optimizer.zero_grad() # set the gradients to 0
 
-                sr_img = model(lr_img, self.magnification_factor).to(self.device)
+                predicted_noise = model(x_t, t, lr_img, self.magnification_factor) 
 
-                train_loss = loss_function(sr_img, hr_img)
+                if loss == 'MSE' or loss == 'MAE' or loss == 'Huber' or loss == 'MSE+Perceptual_noise':
+                    train_loss = loss_function(predicted_noise, noise)
+                elif loss == 'MSE+Perceptual_imgs':
+                    train_loss = loss_function(predicted_noise, noise, hr_img, x_t, self.alpha_hat[t], epoch)
                 
                 train_loss.backward() # compute the gradients
                 optimizer.step() # update the weights
@@ -245,7 +404,7 @@ class SISR_UNet():
                     lr_img = val_loader.dataset[i][0]
                     hr_img = val_loader.dataset[i][1]
 
-                    superres_img = self.sample(model=model, lr_img=lr_img)
+                    superres_img = self.sample(n=1,model=model, lr_img=lr_img, input_channels=lr_img.shape[0], plot_gif_bool=False)
 
                     axs[i,0].imshow(lr_img.permute(1,2,0).cpu().numpy())
                     axs[i,0].set_title('Low resolution image')
@@ -254,8 +413,8 @@ class SISR_UNet():
                     axs[i,2].imshow(superres_img[0].permute(1,2,0).cpu().numpy())
                     axs[i,2].set_title('Super resolution image')
 
-                plt.savefig(os.path.join(os.getcwd(), 'models_run', self.model_name, 'results', f'superres_{epoch}_epoch.png'))
 
+                plt.savefig(os.path.join(os.getcwd(), 'models_run', self.model_name, 'results', f'superres_{epoch}_epoch.png'))
 
             if val_loader is not None:
                 with torch.no_grad():
@@ -264,10 +423,17 @@ class SISR_UNet():
                     for (lr_img,hr_img) in pbar_val:
                         lr_img = lr_img.to(self.device)
                         hr_img = hr_img.to(self.device)
-                        
-                        sr_img = model(lr_img, self.magnification_factor).to(self.device)
 
-                        val_loss = loss_function(sr_img, hr_img)
+                        t = self.sample_timesteps(hr_img.shape[0]).to(self.device)
+                        # t is a unidimensional tensor of shape (images.shape[0] that is the batch_size)with random integers from 1 to noise_steps.
+                        x_t, noise = self.noise_images(hr_img, t) # get batch_size noise images
+                        
+                        predicted_noise = model(x_t, t, lr_img, self.magnification_factor) 
+
+                        if loss == 'MSE' or loss == 'MAE' or loss == 'Huber' or loss == 'MSE+Perceptual_noise':
+                            val_loss = loss_function(predicted_noise, noise)
+                        elif loss == 'MSE+Perceptual_imgs':    
+                            val_loss = loss_function(predicted_noise, noise, hr_img, x_t, self.alpha_hat[t], epoch)
 
                         if verbose:
                             pbar_val.set_postfix(LOSS=val_loss.item()) # set_postfix just adds a message or value
@@ -290,22 +456,6 @@ class SISR_UNet():
                     break
             print('Epochs without improving: ', epochs_without_improving)
 
-    def sample(self, model, lr_img):
-        '''
-        This function samples n images from the model.
-
-        Input:
-            model: the model to sample from
-            lr_img: the low resolution image
-
-        Output:
-            the sampled images
-        '''
-        model.eval()
-        with torch.no_grad():
-            sr_img = model(lr_img[None, ...].to(self.device), self.magnification_factor).to('cpu')
-        return sr_img
-
 def launch(args):
     '''
     This function is the main and call the training, the sampling and all the other functions in the Diffusion class.
@@ -316,14 +466,17 @@ def launch(args):
         batch_size: the batch size
         lr: the learning rate
         epochs: the number of epochs
+        noise_schedule: the noise schedule (linear, cosine)
         save_every: specifies the frequency, in terms of epochs, at which the model weights will be saved
         snapshot_name: the name of the snapshot file
         snapshot_folder_path: the folder path where the snapshots will be saved
         model_name: the name of the model
+        noise_steps: the number of noise steps
         device: the device to use (cuda, cpu, mps)
         patience: the number of epochs after which the training will be stopped if the validation loss is increasing
         input_channels: the number of input channels
         output_channels: the number of output channels
+        plot_gif_bool: if True, the function will plot a gif with the generated images for each class
         magnification_factor: the magnification factor (i.e. the factor by which the image is magnified in the super-resolution task)
         loss: the loss function to use
         UNet_type: the type of UNet to use (Attention UNet, Residual Attention UNet)
@@ -336,13 +489,16 @@ def launch(args):
     batch_size = args.batch_size
     lr = args.lr
     epochs = args.epochs
+    noise_schedule = args.noise_schedule
     save_every = args.save_every
     snapshot_name = args.snapshot_name
     snapshot_folder_path = args.snapshot_folder_path
     model_name = args.model_name
+    noise_steps = args.noise_steps
     device = args.device
     patience = args.patience
     input_channels, output_channels = args.inp_out_channels, args.inp_out_channels
+    plot_gif_bool = args.plot_gif_bool
     magnification_factor = args.magnification_factor
     loss = args.loss
     UNet_type = args.UNet_type
@@ -380,10 +536,15 @@ def launch(args):
 
     snapshot_path = os.path.join(snapshot_folder_path, snapshot_name)
 
-    SISR = SISR_UNet(model=model, snapshot_path=snapshot_path, magnification_factor=magnification_factor, device=device, image_size=image_size, model_name=model_name)
+    diffusion = Diffusion(
+        noise_schedule=noise_schedule, model=model,
+        snapshot_path=snapshot_path,
+        noise_steps=noise_steps, beta_start=1e-4, beta_end=0.02, 
+        magnification_factor=magnification_factor,device=device,
+        image_size=image_size, model_name=model_name)
 
     # Training 
-    SISR.train(
+    diffusion.train(
         lr=lr, epochs=epochs, save_every=save_every,
         train_loader=train_loader, val_loader=val_loader, patience=patience, loss=loss, verbose=True)
     
@@ -393,7 +554,7 @@ def launch(args):
         lr_img = train_dataset[i][0]
         hr_img = train_dataset[i][1]
 
-        superres_img = SISR.sample(model=model, lr_img=lr_img)
+        superres_img = diffusion.sample(n=1,model=model, lr_img=lr_img, input_channels=lr_img.shape[0], plot_gif_bool=plot_gif_bool)
 
         axs[i,0].imshow(lr_img.permute(1,2,0).cpu().numpy())
         axs[i,0].set_title('Low resolution image')
@@ -413,12 +574,15 @@ if __name__ == '__main__':
     parser.add_argument('--image_size', type=int)
     parser.add_argument('--lr', type=float, default=3e-4)
     parser.add_argument('--save_every', type=int, default=20)
+    parser.add_argument('--noise_schedule', type=str, default='cosine')
     parser.add_argument('--snapshot_name', type=str, default='snapshot.pt')
     parser.add_argument('--model_name', type=str)
+    parser.add_argument('--noise_steps', type=int, default=200)
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--patience', type=int, default=10)
     parser.add_argument('--dataset_path', type=str, default=None)
     parser.add_argument('--inp_out_channels', type=int, default=3) # input channels must be the same of the output channels
+    parser.add_argument('--plot_gif_bool', type=bool, default=False)
     parser.add_argument('--loss', type=str)
     parser.add_argument('--magnification_factor', type=int, default=4)
     parser.add_argument('--UNet_type', type=str, default='Residual Attention UNet') # 'Attention UNet' or 'Residual Attention UNet'
