dc74954a956153fdc61113e0a1f9dd917b1b19a9
Adriano Ettari <adrianoettari@libero.it>
2024-06-18 14:36:59 +0200

    daje

--- superres_and_NDVIgen.py
+++ superres_and_NDVIgen.py
@@ -33,6 +33,8 @@ diffusion = Diffusion(
     image_size=image_size, model_name=model_name, Degradation_type=Degradation_type)
 
 lr_img, hr_img = test_dataset[101]
+lr_img = lr_img.to(device)
+hr_img = hr_img.to(device)
 superres_img = diffusion.sample(n=1,model=model, lr_img=lr_img, input_channels=lr_img.shape[0], plot_gif_bool=True)
 
 superres_img = torch.clamp(superres_img, 0, 1)
@@ -68,70 +70,70 @@ plt.show()
 
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SAR to NDVI sampling %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-import torch
-import os
-from torchvision import transforms
-from UNet_model_SAR_TO_NDVI import Residual_Attention_UNet_SAR_TO_NDVI
-from train_diffusion_SAR_TO_NDVI_COMPLETE import Diffusion
-from utils import get_data_SAR_TO_NDVI
-import matplotlib.pyplot as plt
+# import torch
+# import os
+# from torchvision import transforms
+# from UNet_model_SAR_TO_NDVI import Residual_Attention_UNet_SAR_TO_NDVI
+# from train_diffusion_SAR_TO_NDVI_COMPLETE import Diffusion
+# from utils import get_data_SAR_TO_NDVI
+# import matplotlib.pyplot as plt
 
-test_path = os.path.join('SAR_TO_NDVI_dataset', 'test')
-test_dataset = get_data_SAR_TO_NDVI(test_path)
+# test_path = os.path.join('SAR_TO_NDVI_dataset', 'test')
+# test_dataset = get_data_SAR_TO_NDVI(test_path)
 
-device = 'cpu'
-SAR_channels=2
-NDVI_channels=1
+# device = 'cpu'
+# SAR_channels=2
+# NDVI_channels=1
 
 
-image_size = 128
-noise_schedule = 'cosine'
-noise_steps = 1500
-model_name = 'DDP_Residual_Attention_UNet_SAR_TO_NDVI'
+# image_size = 128
+# noise_schedule = 'cosine'
+# noise_steps = 1500
+# model_name = 'DDP_Residual_Attention_UNet_SAR_TO_NDVI'
 
-model = Residual_Attention_UNet_SAR_TO_NDVI(SAR_channels, NDVI_channels, device).to(device)
+# model = Residual_Attention_UNet_SAR_TO_NDVI(SAR_channels, NDVI_channels, device).to(device)
 
-snapshot_path = os.path.join('models_run', model_name, 'weights', 'snapshot.pt')
+# snapshot_path = os.path.join('models_run', model_name, 'weights', 'snapshot.pt')
 
-diffusion = Diffusion(
-        noise_schedule=noise_schedule, model=model,
-        snapshot_path=snapshot_path,
-        noise_steps=noise_steps, beta_start=1e-4, beta_end=0.02,device=device,
-        image_size=image_size, model_name=model_name,
-        multiple_gpus=False, ema_smoothing=False)
+# diffusion = Diffusion(
+#         noise_schedule=noise_schedule, model=model,
+#         snapshot_path=snapshot_path,
+#         noise_steps=noise_steps, beta_start=1e-4, beta_end=0.02,device=device,
+#         image_size=image_size, model_name=model_name,
+#         multiple_gpus=False, ema_smoothing=False)
 
-SAR_img = test_dataset[101][0]
+# SAR_img = test_dataset[101][0]
 
-NDVI_pred_img = diffusion.sample(n=1,model=model, SAR_img=SAR_img, NDVI_channels=NDVI_channels, plot_gif_bool=False)
+# NDVI_pred_img = diffusion.sample(n=1,model=model, SAR_img=SAR_img, NDVI_channels=NDVI_channels, plot_gif_bool=False)
 
-fig, axs = plt.subplots(2,4, figsize=(15,10))
-title_font = {'family': 'sans-serif', 'weight': 'bold', 'size': 15}
-axs = axs.ravel()
-axs[0].imshow(test_dataset[101][0][0].unsqueeze(0).permute(1,2,0).detach().cpu())
-axs[0].set_title('SAR channel 1', fontdict=title_font)
-axs[1].imshow(test_dataset[101][0][1].unsqueeze(0).permute(1,2,0).detach().cpu())
-axs[1].set_title('SAR channel 2', fontdict=title_font)
-axs[2].imshow(test_dataset[101][1].permute(1,2,0).detach().cpu())
-axs[2].set_title('NDVI real', fontdict=title_font)
-axs[3].imshow(NDVI_pred_img[0].permute(1,2,0).detach().cpu())
-axs[3].set_title('NDVI predicted', fontdict=title_font)
-axs[4].hist(test_dataset[101][0][0].unsqueeze(0).flatten().detach().cpu(), bins=100)
-axs[4].set_title('SAR channel 1 histogram', fontdict=title_font)
-axs[4].set_xlim(0, 1)  # Set x-axis range
-axs[4].set_ylim(0, 800)  # Set y-axis range
-axs[5].hist(test_dataset[101][0][1].unsqueeze(0).flatten().detach().cpu(), bins=100)
-axs[5].set_title('SAR channel 2 histogram', fontdict=title_font)
-axs[5].set_xlim(0, 1)  # Set x-axis range
-axs[5].set_ylim(0, 800)  # Set y-axis range
-axs[6].hist(test_dataset[101][1].flatten().detach().cpu(), bins=100)
-axs[6].set_title('NDVI real histogram', fontdict=title_font)
-axs[6].set_xlim(0, 1)  # Set x-axis range
-axs[6].set_ylim(0, 800)  # Set y-axis range
-axs[7].hist(NDVI_pred_img[0].flatten().detach().cpu(), bins=100)
-axs[7].set_title('NDVI predicted histogram', fontdict=title_font)
-axs[7].set_xlim(0, 1)  # Set x-axis range
-axs[7].set_ylim(0, 800)  # Set y-axis range
-plt.show()
+# fig, axs = plt.subplots(2,4, figsize=(15,10))
+# title_font = {'family': 'sans-serif', 'weight': 'bold', 'size': 15}
+# axs = axs.ravel()
+# axs[0].imshow(test_dataset[101][0][0].unsqueeze(0).permute(1,2,0).detach().cpu())
+# axs[0].set_title('SAR channel 1', fontdict=title_font)
+# axs[1].imshow(test_dataset[101][0][1].unsqueeze(0).permute(1,2,0).detach().cpu())
+# axs[1].set_title('SAR channel 2', fontdict=title_font)
+# axs[2].imshow(test_dataset[101][1].permute(1,2,0).detach().cpu())
+# axs[2].set_title('NDVI real', fontdict=title_font)
+# axs[3].imshow(NDVI_pred_img[0].permute(1,2,0).detach().cpu())
+# axs[3].set_title('NDVI predicted', fontdict=title_font)
+# axs[4].hist(test_dataset[101][0][0].unsqueeze(0).flatten().detach().cpu(), bins=100)
+# axs[4].set_title('SAR channel 1 histogram', fontdict=title_font)
+# axs[4].set_xlim(0, 1)  # Set x-axis range
+# axs[4].set_ylim(0, 800)  # Set y-axis range
+# axs[5].hist(test_dataset[101][0][1].unsqueeze(0).flatten().detach().cpu(), bins=100)
+# axs[5].set_title('SAR channel 2 histogram', fontdict=title_font)
+# axs[5].set_xlim(0, 1)  # Set x-axis range
+# axs[5].set_ylim(0, 800)  # Set y-axis range
+# axs[6].hist(test_dataset[101][1].flatten().detach().cpu(), bins=100)
+# axs[6].set_title('NDVI real histogram', fontdict=title_font)
+# axs[6].set_xlim(0, 1)  # Set x-axis range
+# axs[6].set_ylim(0, 800)  # Set y-axis range
+# axs[7].hist(NDVI_pred_img[0].flatten().detach().cpu(), bins=100)
+# axs[7].set_title('NDVI predicted histogram', fontdict=title_font)
+# axs[7].set_xlim(0, 1)  # Set x-axis range
+# axs[7].set_ylim(0, 800)  # Set y-axis range
+# plt.show()
 
 
 # %%
