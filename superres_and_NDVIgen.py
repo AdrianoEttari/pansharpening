933086e17eeca3c28d9241f25e4495009915ca48
Adriano Ettari <adrianoettari@libero.it>
2024-06-30 13:53:31 +0200

    new

--- superres_and_NDVIgen.py
+++ superres_and_NDVIgen.py
@@ -1,4 +1,3 @@
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% super resolution sampling %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 import torch
 import os
 from utils import get_data_superres
@@ -8,7 +7,13 @@ from train_diffusion_superres_COMPLETE import Diffusion
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 import random
-
+import torch
+import os
+from torchvision import transforms
+from UNet_model_SAR_TO_NDVI import Residual_Attention_UNet_SAR_TO_NDVI
+from train_diffusion_SAR_TO_NDVI_COMPLETE import Diffusion
+from utils import get_data_SAR_TO_NDVI
+import matplotlib.pyplot as plt
 
 def super_resolver(img_path, device, model_name):
         noise_schedule = 'cosine'
@@ -17,7 +22,7 @@ def super_resolver(img_path, device, model_name):
         hr_img = Image.open(img_path)
 
         magnification_factor = int([info[13:] for info in model_name.split('_') if info.startswith('magnification')][0])
-        image_size = int([info[7:] for info in model_name.split('_') if info.startswith('imgsize')][0]) * magnification_factor
+        image_size = int([info[9:] for info in model_name.split('_') if info.startswith('LRimgsize')][0]) * magnification_factor
 
         transform = transforms.Compose([transforms.Resize((image_size, image_size))])
         hr_img = transform(hr_img)
@@ -48,9 +53,6 @@ def super_resolver(img_path, device, model_name):
         superres_img = torch.clamp(superres_img, 0, 1)
 
         return superres_img, lr_img, hr_img
-
-
-
 
 def plot_lr_hr_sr(lr_img, hr_img, sr_img, histogram=True):
         if histogram:
@@ -84,108 +86,99 @@ def plot_lr_hr_sr(lr_img, hr_img, sr_img, histogram=True):
                 axs[2].set_title('super resolution image', fontdict=title_font)
                 plt.show()
 
+def SAR_to_NDVI_generator(SAR_img_path, device, n_generations=1):
+        noise_schedule = 'cosine'
+        noise_steps = 1500
+        SAR_channels=2
+        NDVI_channels=1
+        model_name = 'Residual_Attention_UNet_EMA_imgsize128_UNet_SAR_TO_NDVI'
+        model = Residual_Attention_UNet_SAR_TO_NDVI(SAR_channels, NDVI_channels, device).to(device)
+        snapshot_path = os.path.join('models_run', model_name, 'weights', 'snapshot.pt')
+        image_size = int([info[7:] for info in model_name.split('_') if info.startswith('imgsize')][0])
+
+        print(f'Image size: {image_size}, SAR channels: {SAR_channels}, NDVI channels: {NDVI_channels}')
+
+        SAR_img = torch.load(SAR_img_path)
+        if SAR_img.min() < 0 and SAR_img.min() > -1:
+                SAR_img = (SAR_img + 1) / 2
+        elif SAR_img.min() < -1 or SAR_img.max() > 1:
+               raise ValueError('SAR image values are not in the range [-1, 1]')
+        
+        diffusion = Diffusion(
+                noise_schedule=noise_schedule, model=model,
+                snapshot_path=snapshot_path,
+                noise_steps=noise_steps, beta_start=1e-4, beta_end=0.02,device=device,
+                image_size=image_size, model_name=model_name,
+                multiple_gpus=False, ema_smoothing=False)
+        
+        NDVI_pred_img = diffusion.sample(n=n_generations,model=model, SAR_img=SAR_img, NDVI_channels=NDVI_channels, generate_video=False)
+        return NDVI_pred_img
+
+def plot_SAR_NDVI(SAR_img, NDVI_img, NDVI_pred_img, save_path=None):
+    num_pred_imgs = NDVI_pred_img.shape[0]
+    fig, axs = plt.subplots(1, 3 + num_pred_imgs, figsize=(5 * (2 + num_pred_imgs), 10))
+    title_font = {'family': 'sans-serif', 'weight': 'bold', 'size': 8}
+    axs = axs.ravel()
+
+    axs[0].imshow(SAR_img[0].unsqueeze(0).permute(1, 2, 0).detach().cpu())
+    axs[0].set_title('SAR image 1', fontdict=title_font)
+    axs[0].axis('off')
+    
+    axs[1].imshow(SAR_img[1].unsqueeze(0).permute(1, 2, 0).detach().cpu())
+    axs[1].set_title('SAR image 2', fontdict=title_font)
+    axs[1].axis('off')
+    
+    axs[2].imshow(NDVI_img.permute(1, 2, 0).detach().cpu())
+    axs[2].set_title('NDVI image', fontdict=title_font)
+    axs[2].axis('off')
+    
+    for i in range(num_pred_imgs):
+        axs[3 + i].imshow(NDVI_pred_img[i].permute(1, 2, 0).detach().cpu())
+        axs[3 + i].set_title(f'NDVI pred {i}', fontdict=title_font)
+        axs[3 + i].axis('off')
+    
+    plt.show()
+    if save_path is not None:
+        img_to_save = int(input('Which NDVI pred you want to save? (0, 1, 2, ...): '))
+        fig, axs = plt.subplots(1, 4, figsize=(5, 10))
+        axs = axs.ravel()       
+        title_font = {'family': 'sans-serif', 'weight': 'bold', 'size': 8}
+        axs[0].imshow(SAR_img[0].unsqueeze(0).permute(1, 2, 0).detach().cpu())
+        axs[0].set_title('SAR image 1', fontdict=title_font)
+        axs[0].axis('off')
+        axs[1].imshow(SAR_img[1].unsqueeze(0).permute(1, 2, 0).detach().cpu())
+        axs[1].set_title('SAR image 2', fontdict=title_font)
+        axs[1].axis('off')
+        axs[2].imshow(NDVI_img.permute(1, 2, 0).detach().cpu())
+        axs[2].set_title('NDVI image', fontdict=title_font)
+        axs[2].axis('off')
+        axs[3].imshow(NDVI_pred_img[img_to_save].permute(1, 2, 0).detach().cpu())
+        axs[3].set_title(f'NDVI pred', fontdict=title_font)
+        axs[3].axis('off')
+        plt.savefig(f'{save_path}', dpi=300, bbox_inches='tight', pad_inches=0)
+    
 
 if __name__ == '__main__':
         device = 'mps'
-        img_path = os.path.join('imgs_sample','up42_sample_lr.png')
-        model_name = 'DDP_Residual_Attention_UNet_superres_magnification2_LRimgsize128_up42_sentinel2_patches_downblur'
-        superres_img, lr_img, hr_img = super_resolver(img_path, device, model_name)
-        plot_lr_hr_sr(lr_img, hr_img, superres_img, histogram=False)
+        # img_path = os.path.join('imgs_sample','up42_sample_lr.png')
+        # model_name = 'Residual_Attention_UNet_superres_magnification2_LRimgsize128_up42_sentinel2_patches_downblur'
+        # superres_img, lr_img, hr_img = super_resolver(img_path, device, model_name)
+        # plot_lr_hr_sr(lr_img, hr_img, superres_img, histogram=False)
+
+        test_path = os.path.join('imgs_sample', 'test_SAR_TO_NDVI')
+        list_of_files = ['Victoria_0_20220819_patch_267.pt']
+        SAR_img_path = os.path.join(test_path, 'sar', list_of_files[0])
+        SAR_img = torch.load(SAR_img_path)
+        NDVI_img = torch.load(os.path.join(test_path, 'opt', list_of_files[0]))
+
+        NDVI_pred_img = SAR_to_NDVI_generator(SAR_img_path, device, n_generations=5)
+        destination_path = 'models_run/Residual_Attention_UNet_EMA_imgsize128_UNet_SAR_TO_NDVI/results'
+        save_path = os.path.join(destination_path, f'{list_of_files[0].replace(".pt", ".png")}')
+        plot_SAR_NDVI(SAR_img, NDVI_img, NDVI_pred_img, save_path=save_path)
+        
 
 
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SAR to NDVI sampling %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# import torch
-# import os
-# from torchvision import transforms
-# from UNet_model_SAR_TO_NDVI import Residual_Attention_UNet_SAR_TO_NDVI
-# from train_diffusion_SAR_TO_NDVI_COMPLETE import Diffusion
-# from utils import get_data_SAR_TO_NDVI
-# import matplotlib.pyplot as plt
 
-# device = 'cuda'
-# SAR_channels=2
-# NDVI_channels=1
-
-# image_size = 128
-# noise_schedule = 'cosine'
-# noise_steps = 1500
-# model_name = 'DDP_Residual_Attention_EMA_UNet_SAR_TO_NDVI'
-
-# model = Residual_Attention_UNet_SAR_TO_NDVI(SAR_channels, NDVI_channels, device).to(device)
-
-# snapshot_path = os.path.join('models_run', model_name, 'weights', 'snapshot.pt')
-
-# diffusion = Diffusion(
-#         noise_schedule=noise_schedule, model=model,
-#         snapshot_path=snapshot_path,
-#         noise_steps=noise_steps, beta_start=1e-4, beta_end=0.02,device=device,
-#         image_size=image_size, model_name=model_name,
-#         multiple_gpus=False, ema_smoothing=False)
-
-# test_path = os.path.join('imgs_sample', 'test_SAR_TO_NDVI')
-# list_of_files = ['Victoria_0_20210830_patch_289.pt','Victoria_0_20210719_patch_214.pt',
-#                  'Victoria_0_20180623_patch_62.pt', 'Victoria_2_20201115_patch_44.pt',
-#                 'Victoria_2_20200630_patch_81.pt',
-#                  'Victoria_2_20200730_patch_103.pt', 'Victoria_0_20220819_patch_267.pt']
-
-# os.makedirs(os.path.join('imgs_sample', 'generated_SAR_TO_NDVI'), exist_ok=True)
-
-# for i in range(len(list_of_files)):
-#     os.makedirs(os.path.join('imgs_sample', 'generated_SAR_TO_NDVI', list_of_files[i]), exist_ok=True)
-#     SAR_img = (torch.load(os.path.join(test_path, 'sar', list_of_files[i]))+1)/2
-#     NDVI_img = (torch.load(os.path.join(test_path, 'opt', list_of_files[i]))+1)/2
-#     NDVI_pred_img = diffusion.sample(n=5,model=model, SAR_img=SAR_img, NDVI_channels=NDVI_channels, generate_video=False)
-#     plt.imshow(SAR_img[0].unsqueeze(0).permute(1,2,0).detach().cpu())
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.savefig(os.path.join('imgs_sample', 'generated_SAR_TO_NDVI', list_of_files[i], f'SAR_0.png'))
-#     plt.imshow(SAR_img[1].unsqueeze(0).permute(1,2,0).detach().cpu())
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.savefig(os.path.join('imgs_sample', 'generated_SAR_TO_NDVI', list_of_files[i], f'SAR_1.png'))
-#     plt.imshow(NDVI_img.permute(1,2,0).detach().cpu())
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.savefig(os.path.join('imgs_sample', 'generated_SAR_TO_NDVI', list_of_files[i], f'NDVI_real.png'))
-#     for j in range(5):
-#         plt.imshow(NDVI_pred_img[j].permute(1,2,0).detach().cpu())
-#         plt.xticks([])
-#         plt.yticks([])
-#         plt.savefig(os.path.join('imgs_sample', 'generated_SAR_TO_NDVI', list_of_files[i], f'NDVI_pred_{j}.png'))
-
-
-# test_dataset = get_data_SAR_TO_NDVI(test_path)
-# SAR_img = test_dataset[101][0]
-# NDVI_img = test_dataset[101][1]
-
-# NDVI_pred_img = diffusion.sample(n=5,model=model, SAR_img=SAR_img, NDVI_channels=NDVI_channels, generate_video=False)
-# fig, axs = plt.subplots(1,5, figsize=(15,10))
-# axs = axs.ravel()
-# for i in range(5):
-#     axs[i].imshow(NDVI_pred_img[i].permute(1,2,0).detach().cpu())
-#     axs[i].set_title(f'NDVI predicted {i}')
-# plt.show()
-
-# fig, axs = plt.subplots(2,4, figsize=(15,10))
-# title_font = {'family': 'sans-serif', 'weight': 'bold', 'size': 15}
-# axs = axs.ravel()
-# axs[0].imshow(SAR_img[0].unsqueeze(0).permute(1,2,0).detach().cpu())
-# axs[0].set_title('SAR channel 1', fontdict=title_font)
-# axs[1].imshow(SAR_img[1].unsqueeze(0).permute(1,2,0).detach().cpu())
-# axs[1].set_title('SAR channel 2', fontdict=title_font)
-# axs[2].imshow(NDVI_img.permute(1,2,0).detach().cpu())
-# axs[2].set_title('NDVI real', fontdict=title_font)
-# axs[3].imshow(NDVI_pred_img[0].permute(1,2,0).detach().cpu())
-# axs[3].set_title('NDVI predicted', fontdict=title_font)
-# axs[4].hist(SAR_img[0].unsqueeze(0).flatten().detach().cpu(), bins=100)
-# axs[4].set_title('SAR channel 1 histogram', fontdict=title_font)
-# axs[5].hist(SAR_img[1].unsqueeze(0).flatten().detach().cpu(), bins=100)
-# axs[5].set_title('SAR channel 2 histogram', fontdict=title_font)
-# axs[6].hist(NDVI_img.flatten().detach().cpu(), bins=100)
-# axs[6].set_title('NDVI real histogram', fontdict=title_font)
-# axs[7].hist(NDVI_pred_img[0].flatten().detach().cpu(), bins=100)
-# axs[7].set_title('NDVI predicted histogram', fontdict=title_font)
-# plt.show()
 
 
 # %%
